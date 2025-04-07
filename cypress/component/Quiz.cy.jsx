77935282870a196e97e3fca752a4b18b851d3c54
carlbr3 <brandon.carlson2089@gmail.com>
2025-04-06 18:27:10 -0700

    updated tests

---
+++ cypress/component/Quiz.cy.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Quiz from '../../src/components/Quiz'
+import '@testing-library/cypress/add-commands'
+
+describe('<Quiz />', () => {
+  const num_questions = 20
+  it('should render a quiz start-up page with a start button', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<Quiz />)
+    
+
+    cy.findByText("Start Quiz").should('be.visible')
+  })
+  it('should render a quiz question when the quiz starts', () => {
+    cy.mount(<Quiz />)
+
+    cy.findbyText("Start Quiz").should('be.visible').click()
+
+    cy.get("h2").should('exist')
+  })
+  it('should show all questions completed by quiz completed page'), () => {
+    cy.mount(<Quiz />)
+
+    cy.findbyText("Start Quiz").should('be.visible').click()
+    for (let i = 0; i < num_questions; i++) {
+      cy.get("h2").should('exist')
+      cy.get("button").first().click()
+    }
+    cy.contains("Your score").should('be.visible')
+    cy.get('button').contains('Take New Quiz').should('be.visible')
+  }
+  it('should start a new quiz'), () => {
+    cy.mount(<Quiz />)
+
+    cy.findbyText("Start Quiz").should('be.visible').click()
+    for (let i = 0; i < num_questions; i++) {
+      cy.get("h2").should('exist')
+      cy.get("button").first().click()
+    }
+    cy.get('button').contains('Take New Quiz').should('be.visible').click()
+  }
+})
